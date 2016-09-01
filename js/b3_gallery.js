667ed6c65be74e37bbf0e09b0efcc8522283091f
Hugo Fittipaldi <hugo.fittipaldi@gmail.com>
2016-09-01 14:50:20 -0300

    Change auto-resize modal

--- js/b3_gallery.js
+++ js/b3_gallery.js
@@ -1,12 +1,11 @@
 ;
 function getItemIndex(id, imgs_width, subtitles) {
     var carousel = jQuery('#carousel-' + id);
-
-    resizeModal(id, imgs_width[0]+'px');
+    resizeModal(id, imgs_width[0]);
     carousel.on('slid.bs.carousel', function() {
         var index = jQuery(carousel).find('figure.active').index(),
             currentIndex = index + 1,
-            tam = imgs_width[index] + 'px';
+            tam = imgs_width[index];
 
         jQuery('#counter-'+id).text(currentIndex);
         jQuery('#caption-'+id).text(subtitles[index]);
@@ -17,7 +16,8 @@ function getItemIndex(id, imgs_width, subtitles) {
 
 function resizeModal(id, tam) {
     var win_size = viewport(),
-        view = win_size['width'];
+        view = win_size['width'],
+        tam = tam + 32 + 'px';
     if (view > 767) {
         jQuery('#galleryModal-' + id).find('.modal-dialog').css({
             width: tam
