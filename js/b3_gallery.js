241262702be63f8aa14b33f5357b154e357ba2e1
Hugo Fittipaldi <hugo.fittipaldi@gmail.com>
2017-09-22 17:29:19 -0300

    Cosmetic changes

--- js/b3_gallery.js
+++ js/b3_gallery.js
@@ -1,35 +1,40 @@
-;
-function getItemIndex(id, imgs_width, subtitles) {
-    var carousel = jQuery('#carousel-' + id);
-    resizeModal(id, imgs_width[0]);
-    carousel.on('slid.bs.carousel', function() {
-        var index = jQuery(carousel).find('figure.active').index(),
-            currentIndex = index + 1,
-            tam = imgs_width[index];
-
-        jQuery('#counter-'+id).text(currentIndex);
-        jQuery('#caption-'+id).text(subtitles[index]);
-
-        resizeModal(id, tam);
-    });
+/*jslint browser: true*/
+/*global $, jQuery*/
+function viewport() {
+    'use strict';
+    var e = window,
+        a = 'inner';
+    if (!window.hasOwnProperty('innerWidth')) {
+        a = 'client';
+        e = document.documentElement || document.body;
+    }
+    return { width: e[a + 'Width'], height: e[a + 'Height'] };
 }
 
 function resizeModal(id, tam) {
+    'use strict';
     var win_size = viewport(),
-        view = win_size['width'],
-        tam = tam + 32 + 'px';
+        view = win_size.width,
+        size = tam + 32 + 'px';
     if (view > 767) {
         jQuery('#galleryModal-' + id).find('.modal-dialog').css({
-            width: tam
+            width: size
         });
     }
 }
 
-function viewport() {
-    var e = window, a = 'inner';
-    if (!('innerWidth' in window )) {
-        a = 'client';
-        e = document.documentElement || document.body;
-    }
-    return { width : e[ a+'Width' ] , height : e[ a+'Height' ] };
+function getItemIndex(id, imgs_width, subtitles) {
+    'use strict';
+    var carousel = jQuery('#carousel-' + id);
+    resizeModal(id, imgs_width[0]);
+    carousel.on('slid.bs.carousel', function () {
+        var index = jQuery(carousel).find('figure.active').index(),
+            currentIndex = index + 1,
+            tam = imgs_width[index];
+
+        jQuery('#counter-' + id).text(currentIndex);
+        jQuery('#caption-' + id).text(subtitles[index]);
+
+        resizeModal(id, tam);
+    });
 }
